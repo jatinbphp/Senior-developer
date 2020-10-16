9ccf0774a56fbebedc59e1730754a307a20a5952
Jatin Bhatt <jatin.b.php@gmail.com>
2020-10-16 14:37:29 +0530

    Commited test after completion

---
+++ fizzBuzz.php
@@ -0,0 +1,20 @@
+<?php
+  	echo "<pre>";
+    $totalNum = 100;
+    for($i=1;$i<=$totalNum;$i++)
+    {
+        if( ($i%3==0) && ($i%5==0) ) {
+           echo "FizzBuzz"."\n";
+
+        }
+        else if($i%3==0){
+           echo "Fizz"."\n";;
+        }
+        else if($i%5==0) {
+           echo "Buzz"."\n";;
+        }
+        else {
+           echo $i."\n";;
+        }
+    }   
+?>
